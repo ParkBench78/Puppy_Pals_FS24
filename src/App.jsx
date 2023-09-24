ff0d7367adb9f36e1acfc682646e4f98b1c1ef4d
Imelda Yoon <136655874+ParkBench78@users.noreply.github.com>
2023-09-24 17:15:20 -0400

    2nd commit Puppy Data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-
+import { puppyList } from "./data.js";
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
