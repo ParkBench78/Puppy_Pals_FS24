346a9eb52b3333594826e55eefea5fbdc6ae296a
Imelda Yoon <136655874+ParkBench78@users.noreply.github.com>
2023-09-26 00:33:09 -0400

    4th commit css completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,50 @@
 import { useState } from "react";
 import "./App.css";
 import { puppyList } from "./data.js";
+import "./index.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+
   console.log("puppyList: ", puppyList);
 
   const [featPupId, setFeatPupId] = useState(null);
-  // function handleClick(puppy) {
-  //   setFeatPupId(puppy.id);
-  //   console.log("puppy id: ", puppy.id);
-  // }
-  // const featuredPup = puppies.find((pup) => {
-  //   pup.id === featPupId;
-  // });
-  // console.log("Featured Puppy: ", featuredPup);
+
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   console.log("featured puppy: ", featuredPup);
+
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p
-            onClick={() => {
-              console.log("puppy id: ", puppy.id);
-              setFeatPupId(puppy.id);
-            }}
-            key={puppy.id}
-          >
-            {puppy.name}
-          </p>
-        );
-      })}
-      {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
+      <header>
+        <span className="title">Puppy Pals</span>
+      </header>
+      <main className="container">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                console.log("puppy id: ", puppy.id);
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </main>
+      <footer className="feature">
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </footer>
     </div>
   );
 }
